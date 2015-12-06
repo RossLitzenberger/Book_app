8700c455db16005befbd24ad63ae4d20b103a1ad
Ross <codingclasses.049@gmail.com>
2015-12-06 09:50:42 -0600

    New app for Review

---
+++ db/migrate/20151206154017_create_books.rb
@@ -0,0 +1,10 @@
+class CreateBooks < ActiveRecord::Migration
+  def change
+    create_table :books do |t|
+      t.string :book_name
+      t.string :author
+
+      t.timestamps null: false
+    end
+  end
+end
