7e54a7aaa1c7bb6e92afeb2ccbfa4d4cf635e94a
Ross <codingclasses.049@gmail.com>
2016-01-13 16:55:36 -0600

    Delete Comments

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -5,4 +5,12 @@ class CommentsController < ApplicationController
 
     redirect_to book_path(@book)
   end
+
+  def destroy
+    @book = Book.find(params[:book_id])
+    @comment = @book.comments.find(params[:id])
+    @comment.destroy
+
+    redirect_to book_path(@book)
+  end
 end
