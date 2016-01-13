b101bc3054ef3194adf1d77e23875e0660349c2c
Ross <codingclasses.049@gmail.com>
2016-01-13 16:24:40 -0600

    Create a Comment migration

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
   root 'books#index'
-  resources :books
+  resources :books do
+    resources :comments
+  end
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160109025200) do
+ActiveRecord::Schema.define(version: 20160113221932) do
 
   create_table "books", force: :cascade do |t|
     t.string   "book_name"
@@ -24,5 +24,15 @@ ActiveRecord::Schema.define(version: 20160109025200) do
     t.integer  "cover_file_size"
     t.datetime "cover_updated_at"
   end
+
+  create_table "comments", force: :cascade do |t|
+    t.string   "name"
+    t.text     "body"
+    t.integer  "book_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  add_index "comments", ["book_id"], name: "index_comments_on_book_id"
 
 end
