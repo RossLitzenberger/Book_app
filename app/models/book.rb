c8a322e2238aaab49b85bc2671748cff1ed3619d
Ross <codingclasses.049@gmail.com>
2016-01-14 11:30:25 -0600

    Add missing image to the project

--- app/models/book.rb
+++ app/models/book.rb
@@ -1,5 +1,5 @@
 class Book < ActiveRecord::Base
   has_many :comments, dependent: :destroy
-  has_attached_file :cover, styles: { medium: "300x300", thumbnail: "50x50" }, default_url: "/images/:style/missing.png"
+  has_attached_file :cover, styles: { medium: "300x300#", thumbnail: "50x50#" }, default_url: "/images/thumbnail/missing.png"
   validates_attachment_content_type :cover, content_type: /\Aimage\/.*\Z/
 end
