270cbbfe4c3a88120a7191a931f139596e203ef9
Ross <codingclasses.049@gmail.com>
2016-01-16 22:51:11 -0600

    Added a new field for username

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,4 +5,8 @@ class User < ActiveRecord::Base
          :recoverable, :rememberable, :trackable, :validatable
   has_many :books
   has_many :comments
+
+  def name
+    "#{first_name} #{last_name}"
+  end
 end
