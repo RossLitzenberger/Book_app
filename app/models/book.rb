8bde2b0fa4ff9182356d416c3b7def622fa5efa7
Ross <codingclasses.049@gmail.com>
2016-01-19 16:03:49 -0600

    Finish up the styling for the index, login and sign in pages.

--- app/models/book.rb
+++ app/models/book.rb
@@ -2,6 +2,6 @@ class Book < ActiveRecord::Base
   acts_as_votable
   belongs_to :user
   has_many :comments, dependent: :destroy
-  has_attached_file :cover, styles: { medium: "300x300#", thumbnail: "50x50#" }, default_url: "/images/thumbnail/missing.png"
+  has_attached_file :cover, styles: { medium: "200x250#", thumbnail: "50x50#" }, default_url: "/images/medium/missing.png"
   validates_attachment_content_type :cover, content_type: /\Aimage\/.*\Z/
 end
